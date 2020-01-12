bd00f42398e0f7f5abb74fa0c29229cac2c526d4
Rain Vagel <rain.vagel@gmail.com>
2020-01-12 23:42:45 +0200

    Added support for two and four sets of moons

--- dataset.py
+++ dataset.py
@@ -1,24 +1,71 @@
+import numpy as np
 from sklearn import datasets
 import tensorflow as tf
 
 
-class CreateDataset:
+class MoonsDataset:
     def __init__(self, dim=2):
         self.dim = dim
 
-    def two_moons(self, n_samples=2000, shuffle=True, noise=.02,
-                  testing_samples=400, testing_shuffle=True, testing_noise=.02):
+    def _generate_moons(self, n_samples=2000, shuffle=True, noise=.02, x_lat_mult=0, x_long_mult=0):
         X, y = datasets.make_moons(n_samples=n_samples, shuffle=shuffle, noise=noise)
         # Rescale and shift the dataset to better fit into zero-one box
         X = (X + 1.6) / 4
         X[:, 0] = X[:, 0] - 0.035
         X[:, 1] = (X[:, 1] - 0.17) * 1.75
+        X[:, 0] = X[:, 0] + x_lat_mult
+        X[:, 1] = X[:, 1] + x_long_mult
+        return X, y
+
+    def two_moons(self, n_samples=2000, shuffle=True, noise=.02,
+                  testing_samples=400, testing_shuffle=True, testing_noise=.02):
+        X, y = self._generate_moons(n_samples, shuffle, noise)
         y = tf.one_hot(y, self.dim)
 
-        X_test, y_test = datasets.make_moons(n_samples=testing_samples, shuffle=testing_shuffle, noise=testing_noise)
-        # Rescale and shift the dataset to better fit into zero-one box
-        X_test = (X_test + 1.6) / 4
-        X_test[:, 0] = X_test[:, 0] - 0.035
-        X_test[:, 1] = (X_test[:, 1] - 0.17) * 1.75
+        X_test, y_test = self._generate_moons(testing_samples, testing_shuffle, testing_noise)
+        return X, y, X_test, y_test
 
+    def two_set_two_moons(self, n_samples=2000, shuffle=True, noise=.02,
+                  testing_samples=400, testing_shuffle=True, testing_noise=.02, position='horizontal'):
+        X, y = self._generate_moons(n_samples, shuffle, noise)
+
+        X_test, y_test = self._generate_moons(testing_samples, testing_shuffle, testing_noise)
+
+        if position == 'horizontal':
+            X_extra, y_extra = self._generate_moons(n_samples, shuffle, noise, 1, 0)
+            X_test_extra, y_test_extra = self._generate_moons(testing_samples, testing_shuffle, testing_noise, 1, 0)
+        elif position == 'vertical':
+            X_extra, y_extra = self._generate_moons(n_samples, shuffle, noise, 0, 1)
+            X_test_extra, y_test_extra = self._generate_moons(testing_samples, testing_shuffle, testing_noise, 0, 1)
+        else:
+            raise Exception("Unknown two moons command")
+
+        X = np.append(X, X_extra, axis=0)
+        y = np.append(y, y_extra, axis=0)
+        X_test = np.append(X_test, X_test_extra, axis=0)
+        y_test = np.append(y_test, y_test_extra, axis=0)
+
+        y = tf.one_hot(y, self.dim)
+
+        return X, y, X_test, y_test
+
+    def four_set_two_moons(self, n_samples=2000, shuffle=True, noise=.02,
+                  testing_samples=400, testing_shuffle=True, testing_noise=.02):
+        X, y = self._generate_moons(n_samples, shuffle, noise)
+
+        X_test, y_test = self._generate_moons(testing_samples, testing_shuffle, testing_noise)
+
+        for x_i in (0, 1):
+            for y_i in (0, 1):
+                if x_i == 0 and y_i == 0:
+                    continue
+                else:
+                    X_extra, y_extra = self._generate_moons(n_samples, shuffle, noise, x_i, y_i)
+                    X_test_extra, y_test_extra = self._generate_moons(testing_samples, testing_shuffle,
+                                                                      testing_noise, x_i, y_i)
+                    X = np.append(X, X_extra, axis=0)
+                    y = np.append(y, y_extra, axis=0)
+                    X_test = np.append(X_test, X_test_extra, axis=0)
+                    y_test = np.append(y_test, y_test_extra, axis=0)
+        y = tf.one_hot(y, self.dim)
         return X, y, X_test, y_test
--- training.py
+++ training.py
@@ -1,8 +1,10 @@
+from pathlib import Path
+
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Activation
 from tensorflow.keras.optimizers import Adam
 
-from dataset import CreateDataset
+from dataset import MoonsDataset
 from models import ModelRunner, MAct, MActModelRunner
 
 
@@ -20,13 +22,18 @@ def create_model():
 
 
 def main():
+    file_name = "codebase_trials/four_moons"
+    run_name = "dense100_dense100_relu_MAct"
+
+    Path(file_name).mkdir(parents=True, exist_ok=True)
     print("Creating model!")
     model = create_model()
     print("Model created!")
-    runner = MActModelRunner("codebase_trials", "dense100_dense100_relu_MAct", 200, 2)
+    runner = MActModelRunner(file_name, run_name, 200, 2)
     optimizer = Adam(learning_rate=0.01)
-    X, y, X_test, y_test = CreateDataset().two_moons()
+    X, y, X_test, y_test = MoonsDataset().four_set_two_moons()
     runner.model_experiment(model=model, optimizer=optimizer, acet=False, X=X, y=y, X_test=X_test, y_test=y_test)
+
 
 if __name__ == "__main__":
     main()
