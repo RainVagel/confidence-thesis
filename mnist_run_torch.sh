bb11ca7430c90bf5601e2097d5433fdbff18b134
Rain Vagel <rain.vagel@gmail.com>
2020-03-21 21:34:32 +0200

    Bash fix

--- mnist_run_torch.sh
+++ mnist_run_torch.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-for i in {1..5}
+for i in {1..5};
 do
-echo "Start training LeNet on MNIST Mact $i"
-python training.py 'MNIST' 'lenet' "exps_paper/mnist_mact_$i" 'True' 'mact' '100'
-echo 'MNIST LeNet trained Mact'
+echo "Start training LeNet on MNIST Mact $i";
+python training.py 'MNIST' 'lenet' "exps_paper/mnist_mact_$i" 'True' 'mact' '100';
+echo 'MNIST LeNet trained Mact';
 done
 
-for i in {1..5}
+for i in {1..5};
 do
-echo "Start training LeNet on MNIST Softmax $i"
-python training.py 'MNIST' 'lenet' "exps_paper/mnist_softmax_$i" 'False' 'softmax' '100'
-echo 'MNIST LeNet trained Softmax'
+echo "Start training LeNet on MNIST Softmax $i";
+python training.py 'MNIST' 'lenet' "exps_paper/mnist_softmax_$i" 'False' 'softmax' '100';
+echo 'MNIST LeNet trained Softmax';
 done
