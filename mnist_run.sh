4ff941e451b0d6c0ad3fc8a1cb7bdbeb7d5fece8
Rain Vagel <rain.vagel@gmail.com>
2020-05-19 20:59:51 +0300

    Removed bash scripts

--- mnist_run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo 'Start training LeNet on MNIST Mact'
-python training.py 'MNIST' 'lenet' 'full_reg' 'True' 'mact' '100'
-echo 'MNIST LeNet trained Mact'
-
-echo 'Start training LeNet on MNIST Softmax'
-python training.py 'MNIST' 'lenet' 'full_reg' 'False' 'softmax' '100'
-echo 'MNIST LeNet trained Softmax'
